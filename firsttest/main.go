750a691caf52b905a0f9864521a5656bae31e84e
masimass <masoumeh83@gmail.com>
2017-11-12 12:43:47 +0330

    my git commist test text

--- firsttest/main.go
+++ firsttest/main.go
@@ -2,7 +2,16 @@ package main
 
 import "fmt"
 
+var b int
+
 func main() {
 
-	fmt.Println("Hiiiii")
+	a := 2
+
+	fmt.Println(a)
+	foo()
+}
+
+func foo() {
+	fmt.Println(b)
 }
---
+++ variables/main.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+var a = "text in a"
+var b = 2
+var c bool
+
+func main() {
+
+	fmt.Println("$v \n", a, b)
+	fmt.Println("%v", c)
+}
